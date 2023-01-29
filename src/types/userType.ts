b9b3a1fe7e61434fc4ed43393d801c724146d4b7
Aleksei Gazov <lekha.gazov@yandex.ru>
2023-01-29 21:02:55 +0300

    first commit

---
+++ src/types/userType.ts
@@ -0,0 +1,28 @@
+export interface UsersState {
+    users: any[]
+    loading: boolean
+    error: null | string
+}
+
+export enum UserActionTypes {
+    FEATCH_USERS = 'FEATCH_USERS',
+    FEATCH_USERS_SUCCESS = 'FEATCH_USERS_SUCCESS',
+    FEATCH_USERS_ERROR = 'FEATCH_USERS_ERROR',
+}
+
+interface FeatchUserAction {
+    type: UserActionTypes.FEATCH_USERS
+}
+
+interface FeatchUserSuccessAction {
+    type: UserActionTypes.FEATCH_USERS_SUCCESS
+    payload: any[]
+}
+
+interface FeatchUserErrorAction {
+    type: UserActionTypes.FEATCH_USERS_ERROR
+    payload: string
+}
+
+
+export type UserAction = FeatchUserAction | FeatchUserSuccessAction | FeatchUserErrorAction
